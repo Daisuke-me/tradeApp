e3dbd96722baa8d414667dc55e4ff9db18587ef4
daisuke <daisuke@daisukenoMacBook-Pro.local>
2020-10-13 00:49:56 +0900

    add_company_list

--- tradeApp/ContentView.swift
+++ tradeApp/ContentView.swift
@@ -29,7 +29,19 @@ struct ContentView: View {
                     Text("検索")
                 }
             }
-            Text(self.TestCompany)
+            Text("検索結果：" + self.TestCompany)
+            Spacer()
+            Text("お気に入り銘柄")
+                .font(.title2)
+                .padding(.horizontal)
+                .border(Color.blue, width: 2)
+            Spacer()
+            CompanyList(Companies: [
+                Company(id: 1, code: "A0030", name: "AAPL", stockPrice: 121.27),
+                Company(id: 2, code: "A3311", name: "AMZN", stockPrice: 126.49),
+                Company(id: 3, code: "A6161", name: "FB", stockPrice: 271.82),
+                Company(id: 4, code: "A4987", name: "GOOGL", stockPrice: 1543.5)
+            ]).border(Color.blue).font(.caption)
         }
     }
 }
